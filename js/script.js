d0d04df2997ef674368e3d9daeccc03f39779855
Jen Janik <jenmjanik@gmail.com>
2021-03-16 18:54:41 -0500

    made each section in grid  clickable with basic functionality to change background color

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
 console.log('script.js is linked');
+
+const gameBoard = document.querySelector('.game-board-grid');
+console.log('uses .childNodes: ' + gameBoard.childNodes);
+console.log('uses .children: ' + gameBoard.children);
+
+gameBoard.childNodes.forEach(child => {
+    child.addEventListener('click', () => {
+        console.log(child.textContent + 'button was selected');
+        child.style.background = 'blue';
+        console.log(child.style.background);
+    })
+});
