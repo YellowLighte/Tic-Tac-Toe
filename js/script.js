fa6e930ad16cbf43e4aecd31ba879becdc03a507
Jen Janik <jenmjanik@gmail.com>
2021-03-17 10:02:09 -0500

    created button to clear game board

--- js/script.js
+++ js/script.js
@@ -1,13 +1,28 @@
 console.log('script.js is linked');
 
 const gameBoard = document.querySelector('.game-board-grid');
-console.log('uses .childNodes: ' + gameBoard.childNodes);
-console.log('uses .children: ' + gameBoard.children);
+const clearBtn = document.querySelector('#clear-btn');
+const squares = document.querySelector('.game-board-grid').querySelectorAll('.game-board-space');
 
-gameBoard.childNodes.forEach(child => {
-    child.addEventListener('click', () => {
-        console.log(child.textContent + 'button was selected');
-        child.style.background = 'blue';
-        console.log(child.style.background);
-    })
+console.log(squares);
+
+clearBtn.addEventListener('click', () => {
+    console.log('board should be cleared');
+    clearBoard();
 });
+
+function createBoard() {
+    gameBoard.childNodes.forEach(child => {
+        child.addEventListener('click', () => {
+            child.style.background = 'blue';
+        })
+    });
+}
+
+function clearBoard() {
+    squares.forEach(square => {
+        square.style.background = 'white';
+    });
+}
+
+createBoard();
