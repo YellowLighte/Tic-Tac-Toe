ebf3396bcc92fffba8160b45b95c58d9bcabd90f
Jen Janik <jenmjanik@gmail.com>
2021-03-17 15:29:57 -0500

    made squares clickable only once

--- js/script.js
+++ js/script.js
@@ -8,18 +8,26 @@ const squares = document.querySelector('.game-board-grid').querySelectorAll('.ga
 const player1_text = 'X';
 const player2_text = 'O';
 let moveCounter = 0;
+let moveMemory = ['', '', '', '', '', '', '', '', '', ''];
 
-// array to hold winning combinations
+const winningCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+let player1Moves = [];
+let player2Moves = [];
 
-// current player starts with player1
+
 let currentPlayer = player1_text;
 
 // track which player selects which square
     // if square has not been selected yet, track that the player has clicked the square
-    // update the square with the player marker
-
-// after each click, check to see if that player has won
-
 
 clearBtn.addEventListener('click', clearBoard);
 
@@ -27,12 +35,24 @@ function createBoard() {
     gameBoard.childNodes.forEach(child => {
         child.addEventListener('click', () => {
             child.innerText = currentPlayer;
+            moveMemory[child.id] = currentPlayer;
+            if (currentPlayer === player1_text) {
+                player1Moves.push(child.id);
+            } else {
+                player2Moves.push(child.id);
+            }
+
+            //check to see if the currentPlayer has won
+            console.log(checkForWin());
+            // if (checkForWin()) {
+            //     console.log('a player has won!');
+            // }// testing to see if winning condition is met
             changePlayer();
             moveCounter++;
-            console.log('total moves: ' + moveCounter);
-        })
+        }, {once: true})
     });
 }
+
 
 function clearBoard() {
     squares.forEach(square => {
@@ -48,8 +68,30 @@ function changePlayer() {
     } else {
         currentPlayer = player2_text;
     }
-    console.log('player has been changed');
-    console.log('current player is: ' + currentPlayer);
+}
+
+function checkForWin() {
+    // if winningCombos contains player1/2Moves
+    // winningCombos.forEach(combo => {
+    //     for (let i = 0; i < player1Moves.length; i++) {
+    //         if (!combo.includes(player1Moves[i])) {
+    //             return false;
+    //         }
+    //         return true;
+    //     }
+    //     for (let i = 0; i < player2Moves.length; i++) {
+    //         if (!combo.includes(player2Moves[i])) {
+    //             return false;
+    //         }
+    //         return true;
+    //     }
+    // })
+    // winningCombos.forEach(combo => {
+    //     if (combo.includes(player1Moves.forEach(move => {
+    //             console.log('a player has won!');
+    //         })
+    //     ))
+    // }})
 }
 
 createBoard();
