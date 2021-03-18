edf108e90e64cc801bb2e816d4251befb99d850c
Jen Janik <jenmjanik@gmail.com>
2021-03-18 11:13:04 -0500

    increased font size for player tokens on game board

--- js/script.js
+++ js/script.js
@@ -1,15 +1,10 @@
 const gameBoard = document.querySelector('.game-board-grid');
 const clearBtn = document.querySelector('#clear-btn');
 const squares = document.querySelector('.game-board-grid').querySelectorAll('.game-board-space');
-
 const player1_text = 'X';
 const player2_text = 'O';
 let moveCounter = 0;
-
-//let moveMemory = new Array(9);
-//TODO ask about this in stand up - win condition will fire with null values
 let moveMemory = [1,2,3,4,5,6,7,8,9];
-
 let currentPlayer = player1_text;
 
 clearBtn.addEventListener('click', clearBoard);
@@ -17,50 +12,34 @@ clearBtn.addEventListener('click', clearBoard);
 function createBoard() {
     currentPlayer = player1_text;
     moveCounter = 0;
-
     gameBoard.childNodes.forEach(child => {
         child.addEventListener('click', squareEventHandling, {once: true})
     });
 }
 
 function squareEventHandling(event) {
-    console.log(event);
     event.target.innerText = currentPlayer;
     moveMemory[event.target.id] = currentPlayer;
     if (moveCounter >= 5) {
         checkForWin();
     }
     changePlayer();
-    console.log(moveMemory);
-    console.log(moveCounter);
 }
 
 function clearBoard() {
     squares.forEach(square => {
         square.innerText = '';
     });
-
-    //TODO Ask about this in stand up
     moveMemory = [1,2,3,4,5,6,7,8,9];
     currentPlayer = player1_text;
     moveCounter = 0;
-    console.log('END CLEARBOARD');
-    console.log(' ');
-    //Destroy event listeners on squares
     squares.forEach(square => {
         square.removeEventListener('click', squareEventHandling);
     });
     createBoard();
-    //window.location.reload(true);
 }
 
 function changePlayer() {
-    // if (moveCounter % 2 === 1){
-    //     currentPlayer = player1_text;
-    // } else {
-    //     currentPlayer = player2_text;
-    // }
-    // moveCounter++;
     currentPlayer = currentPlayer === player1_text? player2_text : player1_text;
     moveCounter++;
 }
